0ebf2eced4a3bf7b8edba4a1d0860ac288ee1afa
cho015-cmd <cho015@gsuite.gen.go.kr>
2025-10-11 09:19:01 +0900

    Update app.py

--- jjs_/app.py
+++ jjs_/app.py
@@ -35,12 +35,13 @@ def read_html_file(file_path):
 if __name__ == "__main__":
     # You can now specify a relative path to the HTML file.
     # 이제 HTML 파일의 상대 경로를 지정할 수 있습니다.
-    html_file_path = "htmls/index.html"
+    html_file_path = "htmls/index3.html"
     html_content = read_html_file(html_file_path)
 
     if html_content:
         # Render the HTML content
         # HTML 콘텐츠를 렌더링합니다.
         st.components.v1.html(html_content, height=800, scrolling=True)
+
 
 
